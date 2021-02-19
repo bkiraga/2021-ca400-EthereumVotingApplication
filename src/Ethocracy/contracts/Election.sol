e3d281d4ce60f07a54bc578347d5cf1f4c8afb6e
kiragab2 <bartlomiej.kiraga2@mail.dcu.ie>
2021-02-19 14:08:02 +0000

    Added the contract changes that i forgot to add in the last commit

---
+++ src/Ethocracy/contracts/Election.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+pragma experimental ABIEncoderV2;
+
+contract Election {
+  struct Party {
+      string name;
+      uint id;
+      uint votes;
+  }
+
+  mapping(address => bool) public voters;
+  mapping(uint => Party) public parties;
+  uint public partyCount = 0;
+  
+  uint startTime = block.timestamp;
+//   uint allowedTime = 30;
+  
+  Party public winningParty;
+  uint public winnerVoteCount = 0;
+
+  constructor(string[] memory partyNames) public {
+      for (uint i = 0; i < partyNames.length; i++) {
+          addParty(partyNames[i]);
+      }
+  }
+
+  function addParty (string memory _name) private {
+      parties[partyCount] = Party(_name, partyCount, 0);
+      partyCount ++;
+  }
+
+  function castVote (uint _id) public {
+    //   require(block.timestamp - startTime < allowedTime);
+      require(!voters[msg.sender]);
+      require(_id >= 0 && _id <= partyCount);
+      parties[_id].votes ++;
+      voters[msg.sender] = true;
+  }
+  
+  function countVotes () public {
+    //   require(block.timestamp - startTime >= allowedTime);
+      for (uint i = 0; i < partyCount; i++){
+          if (parties[i].votes > winnerVoteCount) {
+              winningParty = parties[i];
+              winnerVoteCount = parties[i].votes;
+          }
+      }
+  }
+}
---
+++ src/Ethocracy/contracts/ElectionBuilder.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.8.0;
+pragma experimental ABIEncoderV2;
+
+import "./Election.sol";
+
+contract ElectionBuilder{
+    Election[] public elections;
+    uint public electionCount = 0;
+    
+    function deployElection(string[] memory _partyNames) public {
+        Election election = new Election(_partyNames);
+        elections.push(election);
+        electionCount++;
+    }
+}
